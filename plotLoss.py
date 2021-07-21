5b2b27e2d9252b2c850912be59a32bc7b3569ec4
wenxuanou <wou@andrew.cmu.edu>
2021-07-20 20:01:08 -0400

    adjusted parameters, need to change to more complex task model

--- plotLoss.py
+++ plotLoss.py
@@ -1,10 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-ouiLossPath = "results/oui_train_loss.out"
-generatorLossPath = "results/generator_train_loss_record.out"
-discriminatorLossPath = "results/discriminator_train_loss.out"
-testAccPath = "results/test_accuracy.out"
+ouiLossPath = "results/oui_train_loss_10%data_100epoch.out"
+generatorLossPath = "results/generator_train_loss_10%data_100epoch.out"
+discriminatorLossPath = "results/discriminator_train_loss_10%data_100epoch.out"
+testAccPath = "results/test_accuracy_10%data_100epoch.out"
+
+# ouiLossPath = "results/oui_train_loss.out"
+# generatorLossPath = "results/generator_train_loss.out"
+# discriminatorLossPath = "results/discriminator_train_loss.out"
+# testAccPath = "results/test_accuracy.out"
 
 ouiLoss = np.loadtxt(ouiLossPath, delimiter=',')
 generatorLoss = np.loadtxt(generatorLossPath, delimiter=',')
